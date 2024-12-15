8a156ca2832de95c846926363435a0c8dc861e21
Nikhi <D3649527@live.tees.ac.uk>
2024-12-15 23:46:53 +0000

    new commit

---
+++ app/src/main/java/uk/ac/tees/mad/livepoll/presentation/viewmodel/PollViewModel.kt
@@ -0,0 +1,11 @@
+package uk.ac.tees.mad.livepoll.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PollViewModel @Inject constructor(
+
+) : ViewModel() {
+}
